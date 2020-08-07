454e891048d999b8713eac7c0f56bce11e75cd0a
Adilson Junior <aj.adilsonjunior1@gmail.com>
2020-08-07 03:19:03 -0300

    Atualiza Landing para receber dados do back-end

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
 import './styles.css';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -9,6 +11,16 @@ import giveClassesIcons from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 const Landing: React.FC = () => {
+	const [totalConnections, setTotalConnections] = useState(0);
+
+	useEffect(() => {
+		api.get('/connections')
+		.then(response => {
+			const { total } = response.data;
+			setTotalConnections(total);
+		})
+	}, [])
+
 	return(
 		<div id="page-landing">
 			<div id="page-landing-content" className="container">
@@ -36,7 +48,7 @@ const Landing: React.FC = () => {
 				</div>
 
 				<span className="total-connections">
-					Total de 200 conexões já realizadas 
+					Total de {totalConnections} conexões já realizadas 
 					<img src={purpleHeartIcon} alt="Coração roxo"/>
 				</span>
 			</div>
