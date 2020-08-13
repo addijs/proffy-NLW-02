03c2aec28cb72204f8def4d773f69b830d377959
Adilson Junior <aj.adilsonjunior1@gmail.com>
2020-08-13 00:16:30 -0300

    Adiciona variáveis de ambiente

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,6 @@
 import path from 'path';
-import dotenv from 'dotenv'
-dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config()
 
 module.exports = {
 	test: {
@@ -33,21 +33,26 @@ module.exports = {
 		},
 
 		useNullAsDefault: true
-	}
+	},
 
-	// production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user: 'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+	production: {
+    client: 'pg',
+    connection: {
+			host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+		migrations: {
+			directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+			extension: 'ts'
+		},
+		seeds: {
+			directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+		}
+  }
 }
