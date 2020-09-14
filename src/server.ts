5e56f6e541b729903e6ec00984b70ca60d892ec5
Adilson Junior <aj.adilsonjunior1@gmail.com>
2020-09-14 03:15:44 -0300

    Adiciona Graceful Shutdown no server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,45 @@
-import ServerSetup from './app';
+import { ServerSetup } from './app';
+
+enum ExitStatus {
+	Failure = 1,
+	Success = 0
+}
+
+// Tratando de promisses não resolvidas
+
+process.on('unhandledRejection', (reason, promise) => {
+	console.log(`App exiting due to an unhandled promise: ${promise} and reason ${reason}`);
+	throw reason;
+});
+
+// Tratando de qualquer exception não tratada ocorrida na app
+
+process.on('uncaughtException', error => {
+	console.log(`App exiting due to an uncaught exception: ${error}`);
+	process.exit(ExitStatus.Failure);
+});
 
 (async () => {
-	const server = new ServerSetup();
-	
-	await server.init();
-	server.startApp();
+	try {
+		const setup = new ServerSetup();
+		await setup.init();
+		setup.startApp();
+
+		const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+
+		exitSignals.forEach(signal => process.on(signal, async () => {
+			try {
+				await setup.close();
+				console.log('\nApp exited successfully');
+				process.exit(ExitStatus.Success);
+			} catch (err) {
+				console.log(`\nApp exited with error: ${err}`);
+				process.exit(ExitStatus.Failure);
+			}
+		}))
+	} catch (err) {
+		console.log(err);
+		console.log(`\nApp exited with error: ${err}`);
+		process.exit(ExitStatus.Failure);
+	}
 })();
